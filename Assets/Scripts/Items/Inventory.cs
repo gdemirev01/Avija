e48452f537f0d6a7be31a4506b31f28825eb86de
Gdemirev01 <gdemirev78@gmail.com>
2019-12-25 23:28:40 +0200

    items objects and inventory created.

---
+++ Assets/Scripts/Items/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("There is another instance of inventory");
+            return;
+        }
+
+        instance = this;
+    }
+    #endregion
+
+    private int space = 20;
+
+    List<Item> items = new List<Item>();
+
+    public void Add(Item item)
+    {
+        if(items.Count >= space)
+        {
+            Debug.Log("Inventory Full");
+            return;
+        }
+        items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+    }
+}
---
+++ Assets/Scripts/Items/PickupItems.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItems : Interactable
+{
+
+    public Item item;
+
+    public override void Interact()
+    {
+        Inventory.instance.Add(item);
+        Destroy(gameObject);
+    }
+}
